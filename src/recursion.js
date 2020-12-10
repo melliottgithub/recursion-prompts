b07a949ffeb95cde710d6e67d2ca53859de46edc
Michael Elliott <kllmmc23@gmail.com>
2020-12-10 15:14:29 -0700

    exponent

--- src/recursion.js
+++ src/recursion.js
@@ -98,7 +98,16 @@ var range = function (x, y) {
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
-var exponent = function(base, exp) {
+var exponent = function (base, exp) {
+    if (exp === 0) {
+        return 1;
+    } else if(exp > 0) {
+        result = base  * exponent(base, exp - 1);
+    } else {
+        exp = exp * -1;
+        result = 1 / (base * exponent(base, exp - 1));
+    }
+    return result;
 };
 
 // 8. Determine if a number is a power of two.
