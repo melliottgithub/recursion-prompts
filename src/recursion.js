c2898161101456582b7566c4daccfac3e8df2b8c
Michael Elliott <kllmmc23@gmail.com>
2020-12-10 14:05:52 -0700

    isEven

--- src/recursion.js
+++ src/recursion.js
@@ -27,11 +27,28 @@ var sum = function (array) {
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
+var arraySum = function (array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++){
+        if (!Array.isArray(array[i])) {
+            sum += array[i];
+        } else {
+            sum += arraySum(array[i]);
+        } 
+    }
+    return sum;
 };
 
 // 4. Check if a number is even.
-var isEven = function(n) {
+var isEven = function (n) {
+    n = Math.abs(n);
+    if (n === 0) {
+        return true;
+    } else if (n === 1) {
+        return false;
+    } else {
+        return isEven(n - 2);
+    }
 };
 
 // 5. Sum all integers below a given integer.
