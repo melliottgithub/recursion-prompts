2a3affe6c1f7fea3a6724615dd9d6f90e1610bae
Michael Elliott <kllmmc23@gmail.com>
2021-01-14 10:56:49 -0800

    add more test

--- src/recursion.js
+++ src/recursion.js
@@ -4,7 +4,7 @@
 
 // 1. Calculate the factorial of a number. The factorial of a non-negative integer n,
 // denoted by n!, is the product of all positive integers less than or equal to n.
-// Example: 5! = 5 x 4 x 3 x 2 x 1 = 120 ///
+// Example: 5! = 5 x 4 x 3 x 2 x 1 = 120 ////
 // factorial(5); // 120
 var factorial = function (n) {
     if (n < 0) return null;
